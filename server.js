28ce758960def69052fc92c981400a75a36d03b7
John Roche <roche.johnjr@gmail.com>
2017-08-08 18:20:24 -0400

    linking form to database - add new user

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 const pgPromise = require('pg-promise')();
 const db = pgPromise({ database : 'robotsDatabase'});
+const bodyParser = require('body-parser');
 
 app.engine('mst', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mst');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   db.any('SELECT * FROM "robots"').then(robotdata => {
@@ -19,13 +23,29 @@ app.get('/', (req, res) => {
 app.get('/info/:id', (req, res) => {
   const id = req.params.id;
   db.one('SELECT * FROM "robots" WHERE id = $1', [id])
-
   .then(robotdata => {
   res.render('info', robotdata)
 })
   .catch(robotdata => {
     res.render('form', robotdata);
   });
+});
+
+app.post('/form', (req, res) => {
+  const insertRobot = {
+    username: req.body.username,
+    email: req.body.email,
+    university: req.body.university,
+    job: req.body.job
+  }
+  db.one(`INSERT INTO "robots" (username, email, university, job)
+    VALUES($(username), $(email), $(university), $(job)) RETURNING id`,
+    insertRobot;
+  )
+    .then(insertRobotId => {
+      robot_id: insertRobotId.id
+  })
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
